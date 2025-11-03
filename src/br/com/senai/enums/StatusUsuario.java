5c5743b2fc7063e02fa82d34711268067596e5e4
Lucas <lucas_a_mader@estudante.sesisenai.org.br>
2025-11-03 19:39:04 -0300

    deletados arquivos antigos

--- src/br/com/senai/enums/StatusUsuario.java
+++
@@ -1,5 +0,0 @@
-package br.com.senai.enums;
-
-public enum StatusUsuario {
-    ATIVO, DESATIVO;
-}
--- src/br/com/senai/utils/UUIDGenerator.java
+++
@@ -1,8 +0,0 @@
-package br.com.senai.utils;
-import java.util.UUID;
-
-public class UUIDGenerator {
-    public static String generateUUID() {
-        return String.valueOf(UUID.randomUUID());
-    }
-}
