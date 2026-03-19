266dd42cc7cdf56211e74a669d4227bfc738c260
Kgohl <cadugohl333@gmail.com>
2026-03-19 19:31:11 -0300

    uptime implementation

--- src/main/java/br/com/senai/ClientServerApplication.java
+++ src/main/java/br/com/senai/ClientServerApplication.java
@@ -2,8 +2,10 @@ package br.com.senai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ClientServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClientServerApplication.class, args);
