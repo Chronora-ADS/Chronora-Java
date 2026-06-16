e284d42ca9acd85a364d467e6255749f6529eae7
Kgohl <cadugohl333@gmail.com>
2026-06-16 20:30:29 -0300

    fix: ajusta liquibase e seed local

--- src/test/java/br/com/senai/ClientServerApplicationTests.java
+++ src/test/java/br/com/senai/ClientServerApplicationTests.java
@@ -8,13 +8,18 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest(properties = {
         "spring.task.scheduling.enabled=false",
         "app.seed.enabled=false",
-        "spring.datasource.url=jdbc:h2:mem:chronora-test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+        "supabase.url=",
+        "supabase.anon-key=",
+        "supabase.service-role=",
+        "supabase.storage-bucket=service-images",
+        "spring.datasource.url=jdbc:h2:mem:chronora-test;MODE=PostgreSQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
         "spring.datasource.username=sa",
         "spring.datasource.password=",
         "spring.datasource.driver-class-name=org.h2.Driver",
-        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.jpa.hibernate.ddl-auto=validate",
         "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
-        "spring.liquibase.enabled=false"
+        "spring.liquibase.enabled=true",
+        "spring.liquibase.change-log=classpath:db/changelog/db.changelog-master.yaml"
 })
 class ClientServerApplicationTests {
 
