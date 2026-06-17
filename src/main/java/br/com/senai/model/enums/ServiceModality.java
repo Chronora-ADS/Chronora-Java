8c5aaf82cefa67a1c3fa4f0df9b4ab3312108fcd
Isaias Swidzinski Junior <swidzinski.isaias.junior@gmail.com>
2026-06-17 19:27:08 -0300

    Update ServiceModality.java

--- src/main/java/br/com/senai/model/enums/ServiceModality.java
+++ src/main/java/br/com/senai/model/enums/ServiceModality.java
@@ -4,7 +4,12 @@ import lombok.Getter;
 
 @Getter
 public enum ServiceModality {
+<<<<<<< Updated upstream
     PRESENCIAL(0), REMOTO(1);
+=======
+    PRESENCIAL("Presencial"),
+    REMOTO("Remoto");
+>>>>>>> Stashed changes
 
     private final int codigo;
 
@@ -31,10 +36,19 @@ public enum ServiceModality {
             return ServiceModality.PRESENCIAL;
         }
 
+<<<<<<< Updated upstream
         if (normalizedText.equals(ServiceModality.REMOTO.name())) {
             return ServiceModality.REMOTO;
         }
 
         throw new IllegalArgumentException("Modalidade inválida " + text + ".");
+=======
+        return Arrays.stream(values())
+                .filter(modality -> modality.name().equals(normalized))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(
+                        "Modalidade invalida. Use Remoto ou Presencial."
+                ));
+>>>>>>> Stashed changes
     }
 }
