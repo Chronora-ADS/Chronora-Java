2340ae885575cbce46682c2494de3a845c9a2bcc
Fellipe Schiochet <fellipeschiochet090204@gmail.com>
2026-06-22 09:29:49 -0300

    feat(moderator): adiciona aba Chronos com compras e vendas no painel do moderador
    - Novos endpoints GET /moderator/transactions/buy e /sell filtrados por tipo
    - PATCH /moderator/sell/{id}/mark-paid para moderador confirmar PIX enviado
    - PaymentTransactionSummaryDTO agora expoe pixKey para vendas
    - Repository: findAllByTypeOrderByCreatedAtDesc para listar por tipo

--- src/main/java/br/com/senai/controller/ModeratorController.java
+++ src/main/java/br/com/senai/controller/ModeratorController.java
@@ -4,9 +4,12 @@ import br.com.senai.model.DTO.payment.PaymentTransactionSummaryDTO;
 import br.com.senai.model.DTO.payment.PlatformStatsDTO;
 import br.com.senai.model.DTO.service.ModeratorServiceSummaryDTO;
 import br.com.senai.model.DTO.user.UserResponseDTO;
+import br.com.senai.model.enums.PaymentType;
 import br.com.senai.service.payment.ModeratorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +31,29 @@ public class ModeratorController {
             @RequestHeader("Authorization") String tokenHeader
     ) {
         return ResponseEntity.ok(moderatorService.getAllTransactions(tokenHeader));
+    }
+
+    @GetMapping("/transactions/buy")
+    public ResponseEntity<List<PaymentTransactionSummaryDTO>> getBuyTransactions(
+            @RequestHeader("Authorization") String tokenHeader
+    ) {
+        return ResponseEntity.ok(moderatorService.getTransactionsByType(tokenHeader, PaymentType.BUY));
+    }
+
+    @GetMapping("/transactions/sell")
+    public ResponseEntity<List<PaymentTransactionSummaryDTO>> getSellTransactions(
+            @RequestHeader("Authorization") String tokenHeader
+    ) {
+        return ResponseEntity.ok(moderatorService.getTransactionsByType(tokenHeader, PaymentType.SELL));
+    }
+
+    @PatchMapping("/sell/{id}/mark-paid")
+    public ResponseEntity<Void> markSellAsPaid(
+            @RequestHeader("Authorization") String tokenHeader,
+            @PathVariable Long id
+    ) {
+        moderatorService.markSellAsPaid(tokenHeader, id);
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/stats")
